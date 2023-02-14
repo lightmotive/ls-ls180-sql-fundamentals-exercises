77e9ee811c419be9a4e4068519d040a084547928
lightmotive <7719153+lightmotive@users.noreply.github.com>
2023-02-14 02:50:46 +0000

    Complete DML, DDL, DCL Exercise 1

---
+++ 02_DML_DDL_DCL/01_part1.sql
@@ -0,0 +1,14 @@
+-- Data Definition Language (DDL)
+-- A SQL sub-language for creating and managing a relational databases and their schemata.
+-- Common statement keywords:
+CREATE DATABASE
+ALTER TABLE
+DROP DATABASE
+
+-- Data Manipulation Language (DML)
+-- A SQL sub-language for CRUD operations: Create, Retrieve, Update, Delete.
+-- Common statement keywords:
+INSERT INTO
+SELECT
+UPDATE
+DELETE FROM
