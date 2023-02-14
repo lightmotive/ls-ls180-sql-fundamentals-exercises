739cbfe7157ee61ddd58e3c578a8b17c1118ec4c
lightmotive <7719153+lightmotive@users.noreply.github.com>
2023-02-14 02:58:10 +0000

    Add statement examples

--- 02_DML_DDL_DCL/01_part1.sql
+++ 02_DML_DDL_DCL/01_part1.sql
@@ -1,14 +1,39 @@
 -- Data Definition Language (DDL)
 -- A SQL sub-language for creating and managing a relational databases and their schemata.
 -- Common statement keywords:
-CREATE DATABASE
-ALTER TABLE
-DROP DATABASE
+-- CREATE DATABASE
+-- ALTER TABLE
+-- DROP DATABASE
+-- Examples:
+CREATE TABLE
+  cocoa (id int GENERATED ALWAYS AS IDENTITY PRIMARY KEY, name varchar(100) NOT NULL);
+
+ALTER TABLE cocoa
+ADD COLUMN description text;
 
 -- Data Manipulation Language (DML)
 -- A SQL sub-language for data CRUD operations: Create, Retrieve, Update, Delete.
 -- Common statement keywords:
+-- INSERT INTO
+-- SELECT
+-- UPDATE
+-- DELETE FROM
+-- Examples:
 INSERT INTO
+  cocoa
+VALUES
+  (
+    DEFAULT,
+    'Tanzania - Kokoa Kamili',
+    'Flavor Notes: The aroma is a clear, deep, resonating raisin and chocolate. There are additional base notes of burnt sugar and tight dark fruit.'
+  ),
+  (
+    DEFAULT,
+    'Bolivia - Rio Beni Wild Harvest',
+    'Flavor Notes: The chocolate aroma is bold and undeniably chocolate and fresh leaf tobacco and green walnuts.'
+  );
+
 SELECT
-UPDATE
-DELETE FROM
+  *
+FROM
+  cocoa;
